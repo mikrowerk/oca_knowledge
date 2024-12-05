929ba29ee05dc7be1179ac7f2344bd88af6c6b09
Guenther Froestl <guenther@mikrowerk.com>
2024-12-05 15:28:39 +0100

    extended attachment handling, kanban view

--- document_url/models/ir_attachment.py
+++ document_url/models/ir_attachment.py
@@ -1,13 +1,21 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import models, api, fields
 from odoo.exceptions import UserError
 import pprint
 
 
 class IrAttachmentExtended(models.Model):
     _inherit = "ir.attachment"
+
+    original_url = fields.Char(string='Original Url')
+    has_original_url = fields.Boolean(string='Has Original Url', compute='_compute_original_url', store=False)
+
+    @api.depends('original_url', 'url')
+    def _compute_original_url(self):
+        for record in self:
+            record.has_original_url = not record.url and record.original_url and len(record.original_url) > 0
 
     def _compute_mimetype(self, values):
         if values.get("url") and values.get("type", "url") == "url":
